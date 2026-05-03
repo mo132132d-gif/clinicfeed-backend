bedd666e117f5decc8c0eafea638cb367388c079
Mohammed Alsaif <mo132132d@gmail.com>
2026-05-03 11:57:21 +0300

    log backend errors for debugging

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,79 +1,23 @@
-const { env } = require('../config/env');
-const { HttpError } = require('../utils/httpError');
+function errorHandler(err, req, res, next) {
+  console.error("ERROR_HANDLER:", {
+    message: err.message,
+    stack: err.stack,
+    code: err.code,
+    detail: err.detail,
+    hint: err.hint,
+    table: err.table,
+    constraint: err.constraint,
+    path: req.path,
+    method: req.method,
+  });
 
-function statusFromPostgresError(error) {
-  if (error.code === '23505') {
-    return 409;
-  }
+  const statusCode = err.statusCode || err.status || 500;
 
-  if (error.code === '23503' || error.code === '23514' || error.code === '22P02') {
-    return 400;
-  }
-
-  return 500;
-}
-
-function messageFromPostgresError(error) {
-  if (error.code === '23505') {
-    return 'Duplicate value violates a unique constraint';
-  }
-
-  if (error.code === '23503') {
-    return 'Referenced record does not exist';
-  }
-
-  if (error.code === '23514') {
-    return 'Value violates a database constraint';
-  }
-
-  if (error.code === '22P02') {
-    return 'Invalid input syntax';
-  }
-
-  return 'Internal server error';
-}
-
-function errorHandler(error, req, res, next) {
-  if (res.headersSent) {
-    next(error);
-    return;
-  }
-
-  const isHttpError = error instanceof HttpError;
-  const isPostgresError = Boolean(error.code);
-  const statusCode = isHttpError
-    ? error.statusCode
-    : isPostgresError
-      ? statusFromPostgresError(error)
-      : 500;
-
-  const response = {
+  res.status(statusCode).json({
     error: {
-      message: isHttpError
-        ? error.message
-        : isPostgresError
-          ? messageFromPostgresError(error)
-          : 'Internal server error'
-    }
-  };
-
-  if (isHttpError && error.details) {
-    response.error.details = error.details;
-  }
-
-  if (isPostgresError && !env.isProduction) {
-    response.error.details = {
-      code: error.code,
-      detail: error.detail,
-      constraint: error.constraint
-    };
-  }
-
-  if (!env.isProduction && !isHttpError && !isPostgresError) {
-    response.error.details = error.message;
-  }
-
-  res.status(statusCode).json(response);
+      message: statusCode === 500 ? "Internal server error" : err.message,
+    },
+  });
 }
 
 module.exports = { errorHandler };
